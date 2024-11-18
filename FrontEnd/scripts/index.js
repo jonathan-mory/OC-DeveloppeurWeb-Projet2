c9125d59e1e0a56a19f5fc7fc163298801ec0f88
Jonathan Mory <jomory@orange.fr>
2024-11-18 12:47:49 +0100

    Étape 1.2 : Réalisation du filtre des travaux : Ajout des filtres pour afficher les travaux par catégorie

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,12 +1,12 @@
 // Récupération des projets de l'architecte depuis le back-end
-const responseAPI = await fetch("http://localhost:5678/api/works");
-const works = await responseAPI.json();
+const worksApiResponse = await fetch("http://localhost:5678/api/works");
+const works = await worksApiResponse.json();
+const categoriesApiResponse = await fetch("http://localhost:5678/api/categories")
+const categories = await categoriesApiResponse.json();
 
-// Déclaration de la boucle pour parcourir tous les travaux de l'architecte et les ajouter à la galerie
-for (let i = 0; i < works.length; i++) {
-
-    // Récupération de l'élément du DOM ("Gallery") qui accueillera les projets
-    const gallery = document.querySelector(".gallery");
+function generateWorks(works) {
+    // Déclaration de la boucle pour parcourir tous les travaux de l'architecte et les ajouter à la galerie
+    for (let i = 0; i < works.length; i++) {
 
     // Création de la basile HTML figure et intégration du contenu en HTML 
     const figure = document.createElement("figure");
@@ -16,7 +16,55 @@ for (let i = 0; i < works.length; i++) {
     <figcaption>${works[i].title}</figcaption>
     `;
 
+    // Récupération de l'élément du DOM ("Gallery") qui accueillera les projets
+    const gallery = document.querySelector(".gallery");
     // Rattachement de la balise figure au parent "Gallery"
     gallery.appendChild(figure);
+};
 }
 
+// Premier affichage de la page 
+generateWorks(works);
+
+
+// Création et paramètrage du bouton de filtre "Tous" 
+const filterAllBtn = document.createElement("button");
+filterAllBtn.innerText = "Tous";
+const filtersContainer = document.querySelector(".filters-container");
+filtersContainer.appendChild(filterAllBtn);
+filterAllBtn.addEventListener("click", () => {
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(works);
+})
+
+// Création des boutons de filtre "Objets", "Appartements" et "Hotels & Restaurants"
+for (let i = 0; i < categories.length; i++) {
+    const filterBtns = document.createElement("button");
+    filterBtns.id = categories[i].id;
+    filterBtns.innerText = categories[i].name;
+    filtersContainer.appendChild(filterBtns);
+};
+
+// Ajout de l'écouteur d'événement pour trier les objets
+const objectsButton = document.getElementById("1");
+objectsButton.addEventListener("click", () => {
+    const objectWorks = works.filter(work => work.categoryId === 1);
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(objectWorks);
+    });
+
+// Ajout de l'écouteur d'événement pour trier les appartements
+const flatsButton = document.getElementById("2");
+flatsButton.addEventListener("click", () => {
+    const flatWorks = works.filter(work => work.categoryId === 2);
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(flatWorks);
+    })
+
+// Ajout de l'écouteur d'événement pour trier les hotels & restaurants
+const hotelsAndRestaurantsButton = document.getElementById("3");
+hotelsAndRestaurantsButton.addEventListener("click", () => {
+    const hotelAndRestaurantWorks = works.filter(work => work.categoryId === 3);
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(hotelAndRestaurantWorks);
+}) 
